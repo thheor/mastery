4cbb9726068bbb2ba15d36539b6b43c9d5c87b9a
Theo <prstylukman@gmail.com>
2026-02-03 12:28:07 +0700

    fetch data for user's name

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,24 +2,22 @@ import { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient.js';
 import { useNavigate } from 'react-router-dom';
 
-export function Login({ setUser }) {
-  const [name, setName] = useState('');
+export function Login({ setUser, setName, setCorrect, setIncorrect }) {
   const [isLoading, setIsLoading] = useState(false);
+  const [loginName, setLoginName] = useState('');
   const navigate = useNavigate();
 
   async function getData(e){
     e.preventDefault();
-    if(!name){
+    if(!loginName){
       return alert('Please enter your name');
     }
 
     setIsLoading(true);
     const { data, error } = await supabase.from('user').select('*').eq('name', name).single();
 
-    let userId;
-
     if (data){
-      userId = data.id;  
+      console.log(data)
     } else {
       const { data, error } = await supabase.from('user').insert({name: name}).select().single();
       console.log(data)
@@ -27,11 +25,17 @@ export function Login({ setUser }) {
         return console.log(error);
       }
 
-      userId = data.id;
       console.log('new user created')
     }
 
+    let userId = data.id;
+    let correct = data.correct;
+    let incorrect = data.incorrect;
+
     setUser(userId);
+    setName(loginName);
+    setCorrect(correct);
+    setIncorrect(incorrect);
 
     navigate('/home');
 
@@ -42,7 +46,7 @@ export function Login({ setUser }) {
       <h1 className="text-4xl font-bold text-ctp-blue -mt-40">Login Sek</h1>
       <form onSubmit={getData} className="flex flex-col gap-5 w-80 mt-10 text-poppins ">
           <label htmlFor="name" className="text-ctp-text font-medium text-xl">Name</label>
-          <input type="text" name="name" placeholder="Your name" onChange={(e) => setName(e.target.value)}
+          <input type="text" name="name" placeholder="Your name" onChange={(e) => setLoginName(e.target.value)}
           className="-mt-3 text-xl bg-ctp-text text-ctp-base py-2 pl-2 focus:outline-none placeholder:text-base  rounded" />
           <button type="submit"  className="p-2 text-xl bg-ctp-lavender cursor-pointer hover:bg-ctp-blue rounded">
           {isLoading ? <span>Loading...</span> : <span>Login</span>}</button>
