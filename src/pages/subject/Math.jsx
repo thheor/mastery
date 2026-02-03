5e55c3d6fb41479fa6df767feb3a6d17f3b392a2
Theo <prstylukman@gmail.com>
2026-02-03 12:29:55 +0700

    initial template for math practice

--- src/pages/subject/Math.jsx
+++ src/pages/subject/Math.jsx
@@ -1,20 +1,12 @@
-import { useState } from "react";
-import { data } from "../../data/math.json"; 
+import { useState } from 'react';
+import { Card } from '../../components/Card.jsx'
 
 export function Math({user}) {
-  const [current, setCurrent] = useState(0);
   
   return(
-  <div className="min-h-screen bg-ctp-base font-poppins text-ctp-text flex flex-col items-center">
-    <h1 className="mt-30 text-4xl font-semibold">Pengetahuan Kuantitatif</h1>
-    <div className="mt-10 w-200 ">
-      <p className="text-2xl font-medium">Soal {current}</p>
-      <div className="flex">
-        <p className="text-xl ">Apakah bumi itu bulat?</p>
-        <input type="text" name="answer" className="px-2 text-lg bg-ctp-text text-ctp-base focus:outline-none ml-2 rounded" />
-      </div>
-    </div>
-  </div>
+    <section id="link" className="min-h-screen bg-ctp-base flex flex-col gap-5 justify-center items-center">
+      <Card link={'practice/english'} title="English" description="Learn english grammar" />
+      <Card link={'practice/math'} title="Math" description="Learn math " />
+    </section>
   );
 }
-
