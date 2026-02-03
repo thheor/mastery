98e777d4dd9ca6d06efc145af4143da76c4c2e9c
Theo <prstylukman@gmail.com>
2026-02-03 12:30:49 +0700

    add route for profile

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ProtectedRoute } from './components/ProtectedRoute.jsx'
 import { Login } from './pages/Login.jsx'
+import { Profile } from './pages/Profile.jsx'
 import { Dashboard } from './pages/Dashboard.jsx'
 import { CourseList } from './pages/CourseList.jsx'
 import { Practice } from './pages/Practice.jsx'
@@ -12,12 +13,16 @@ import { Navbar } from './components/Navbar.jsx'
 
 function App() {
   const [user, setUser] = useState(null);
+  const [name, setName] = useState(null);
+  const [correct, setCorrect] = useState(null);
+  const [incorrect, setIncorrect] = useState(null);
 
   return (
     <BrowserRouter>
-      {user && <Navbar />}
+      {user && <Navbar user={user} name={name} />}
       <Routes>
-        <Route path="/" element={<Login setUser={setUser} />} />
+        <Route path="/" element={<Login setUser={setUser} setName={setName} setCorrect={setCorrect} setIncorrect={setIncorrect} />} />
+        <Route path="/profile" element={<ProtectedRoute user={user} ><Profile user={user} name={name} correct={correct} incorrect={incorrect}  /></ProtectedRoute>} />} />
         <Route path="/home" element={<ProtectedRoute user={user} ><Dashboard user={user} /></ProtectedRoute>} />} />
         <Route path="/course" element={<ProtectedRoute user={user} ><CourseList user={user} /></ProtectedRoute>} />} />
         <Route path="/practice" element={<ProtectedRoute user={user} ><Practice user={user} /></ProtectedRoute>} />} />
