e9f923836acf23317555a263e040fd4cbf23bca0
Theo <prstylukman@gmail.com>
2026-02-03 12:25:08 +0700

    add hook for floating panel

--- src/hooks/utils.js
+++ src/hooks/utils.js
@@ -14,3 +14,24 @@ export function useLocalStorage(key) {
 
   return [value, setValue];
 }
+
+export function useFloatingPanel(ref, handler){
+  useEffect(() => {
+    const listener = (e) => {
+      if (!ref.current || ref.current.contains(e.target)) {
+        return;
+      }
+
+      handler(e);
+    }
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    }
+
+  }, [ref, handler])
+}
