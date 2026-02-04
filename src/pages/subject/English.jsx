771baac490f79c10fd35b4ca5613e30fb2e95ca8
Theo <prstylukman@gmail.com>
2026-02-04 22:22:41 +0700

    add supabase function to update correct and incorrect in db

--- src/pages/subject/English.jsx
+++ src/pages/subject/English.jsx
@@ -4,10 +4,11 @@ import { CreateQuestion } from '../../components/CreateQuestion.jsx';
 import { InputField } from '../../components/InputField.jsx';
 import reactStringReplace from "react-string-replace";
 import { useFloatingPanel } from "../../hooks/utils.js";
+import { supabase } from "../../supabaseClient.js";
 
 export function English({user}) {
   const [questionIndex, setQuestionIndex] = useState(() => {
-    const index = Math.floor(Math.random() * data.length); 
+    const index = Math.floor(Math.random() * 36); 
     return index;
   });
   const [instruction, setInstruction] = useState(() => {
@@ -41,20 +42,27 @@ export function English({user}) {
     setNumber(prev => prev + 1);
   }
 
+  function normalization(str) {
+    return str.toLowerCase().replace(/[^\w\s]/gi, '').replace(/\s+/g, '');
+  }
+
   function checkAnswer(){
-    let key = answerKey.toLowerCase();
-    key = key.split("//");
-    const ans = answer.trim().toLowerCase();
+    let key = normalization(answerKey);
+    key = key.split("_");
+    const ans = normalization(answer);
+    // not using (.) or any tanda baca in the end of the line
+    // typo max 1 per word
 
     if(ans === key[0] || ans === key[1]){
       setCorrect(prev => prev + 1);
-      //const handleDB = async () => {
-        //const { error } = await supabase.from('user').update({correct: correct});
-      //}
+      const handleDB = async (user) => {
+        const { error } = await supabase.rpc('addcorrect', {user_id: user});
+        if(error) console.log(error)
+      }
+      console.log(user)
+      handleDB(user);
       setIsCorrect(true);
     } else {
-      let key = answerKey.split('//');
-      //alert(`The correct answer is "${key[0]}"`);
       setIncorrect(prev => prev + 1);
       setIsCorrect(false);
     }
@@ -63,14 +71,17 @@ export function English({user}) {
 
   function handleSubmit(e){
     e.preventDefault();
+    if(answer === ''){
+      return;
+    }
     checkAnswer();
     setIsPanelOpen(true);
     setAnswer('');
   }
   
   function setUp(){
-    let index = Math.floor(Math.random() * data.length);
-    setQuestionIndex(prev => prev === index ? Math.floor(Math.random() * data.length) : index);
+    let index = Math.floor(Math.random() * 36);
+    setQuestionIndex(prev => prev === index ? Math.floor(Math.random() * 36) : index);
     setQuestion(data[questionIndex].question)
     setInstruction(data[questionIndex].instruction);
     setAnswerKey(data[questionIndex].answer);
@@ -89,7 +100,7 @@ export function English({user}) {
           <p className="text-xl font-semibold ">incorrect</p>
         </div>
       </div>
-    <div className="mt-10 w-80 sm:w-100 md:w-160 lg:w-200 ">
+    <div className={`mt-10 w-80 sm:w-100 md:w-160 lg:w-200 `}>
       <p className="text-4xl font-medium">Soal {number}</p>
       <h1 className="text-ctp-text text-2xl font-semibold">{instruction}</h1>
       <CreateQuestion question={question} keyword="[blank]" handleSubmit={handleSubmit} setAnswer={setAnswer} answer={answer} />
@@ -97,12 +108,12 @@ export function English({user}) {
     </div>
     {isPanelOpen && 
     <div ref={panelRef} className="fixed top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] ">
-      <div className="bg-ctp-lavender w-60 md:w-100 min-h-30 rounded-xl border-2 border-ctp-crust">
+      <div className="bg-ctp-lavender w-60 sm:w-100 min-h-30 rounded-xl border-2 border-ctp-crust">
         <p className={`text-center text-2xl ${isCorrect ? 'text-green-600' : 'text-red-500'} font-poppins font-semibold text-ctp-crust mt-2`}>
           {isCorrect ? 'Correct' : 'Wrong'}
         </p>
-        <p className="text-start px-2 text-base md:text-lg font-poppins font-normal  text-ctp-crust ">The Correct Answer is</p>
-        <p className="text-start px-2 md:px-8 pb-4 text-center text-xl md:text-2xl font-poppins font-medium text-ctp-crust ">{answerKey.replace("//", " OR ")
+        <p className="text-start px-2 sm:ml-6 text-base md:text-lg font-poppins font-normal  text-ctp-crust ">The Correct Answer is</p>
+        <p className="text-start px-2 sm:px-8 pb-4 text-center text-xl md:text-2xl font-poppins font-medium text-ctp-crust ">{answerKey.replace("_", " OR ")
         }</p>
       </div>
       <div className="flex justify-center">
