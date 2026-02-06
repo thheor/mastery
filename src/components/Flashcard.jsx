7462499bb19eab3b1acbc18152520641d5c8c7a5
Theo <prstylukman@gmail.com>
2026-02-06 22:57:21 +0700

    add animation and responsive design

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,13 +1,30 @@
-import { useState, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Icon } from "./Icon.jsx";
 import { motion, AnimatePresence } from "motion/react";
 
-export function Flashcard({title, content, isImage = false, image, isCard = false}) {
+export function Flashcard({title, content, index, isImage = false, image, isCard = false}) {
   const [isVisible, setIsVisible] = useState(true);
   const [startX, setStartX] = useState(0);
   const [startY, setStartY] = useState(0);
+  const [maxX, setMaxX] = useState(500);
+  const [maxY, setMaxY] = useState(200);
+  const [offsetX, setOffsetX] = useState(1000);
+  const [offsetY, setOffsetY] = useState(1000);
+  const [isMobile, setIsMobile] = useState(false);
   const startXRef = useRef(0);
   const startYRef = useRef(0);
+
+  useEffect(() => {
+
+  if(window.innerWidth <= 640){
+    setIsMobile(true);
+    setMaxX(30);
+    setMaxY(80);
+    setOffsetY(100);
+    setOffsetX(100);
+  }
+
+  }, [])
 
   return(
     <AnimatePresence>
@@ -25,12 +42,18 @@ export function Flashcard({title, content, isImage = false, image, isCard = fals
 
         const endPointX = info.point.x;
         const endPointY = info.point.y;
+          console.log('start:' + startXRef.current + 'end' + endPointX)
+          console.log(info.offset)
 
-        if(Math.abs(startYRef.current - endPointY) > 80 || Math.abs(startXRef.current - endPointX) > 100 || Math.abs(info.offset.x) > 100 || Math.abs(info.offset.y) > 100){
+        if(Math.abs(startYRef.current - endPointY) > maxY || Math.abs(startXRef.current - endPointX) > maxX || Math.abs(info.offset.x) > offsetX || Math.abs(info.offset.y) > offsetY){
           setIsVisible(false)
         }
-      }} exit={{opacity: 0, display: 'none', scale: 0.5, transition: { duration: 0.2}}}
-        className={`flex flex-col items-center ${isImage ? '' : 'justify-center'} w-56 sm:w-70 h-88 sm:h-110 bg-ctp-text rounded-xl`}>
+        }} 
+        exit={{opacity: 0, display: 'none', scale: 0.5, transition: { duration: 0.2}}}
+
+        style={{zIndex: index}}
+
+        className={`absolute -translate-x-1/2 -translate-y-1/2 flex flex-col items-center ${isImage ? '' : 'justify-center'} w-56 sm:w-70 h-88 sm:h-110 bg-ctp-text border border-ctp-base rounded-xl`}>
       {isImage && 
         <div className="w-48 sm:w-60 h-40 sm:h-50 mt-5 bg-ctp-blue rounded-xl">
           <img src={image} alt="" className="object-cover w-48 h-40" />
