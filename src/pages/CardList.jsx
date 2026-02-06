15bf203ddc8032367e7c8b13f6023cf9408cfc6f
Theo <prstylukman@gmail.com>
2026-02-06 22:58:02 +0700

    add card list for flashcards

--- src/pages/CardList.jsx
+++ src/pages/CardList.jsx
@@ -6,8 +6,8 @@ export function CardList({user}) {
   
   return(
     <section className="min-h-screen bg-ctp-base flex flex-col gap-5 justify-center items-center">
-      <Card title="Flashcards" description="Better study with flashcards" />
-      <Card title="Create Flashcards" description="Create your own flashcards" />
+      <Card link={'card/flashcards'} title="Flashcards" description="Better study with flashcards" />
+      <Card link={'card/create'} title="Create Flashcards" description="Create your own flashcards" />
     </section>
   );
 }
