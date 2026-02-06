108495d4c39a3d46722996f9a373c60e41a7f886
Theo <prstylukman@gmail.com>
2026-02-06 23:00:56 +0700

    add page for create flashcards and view flashcards

---
+++ src/pages/flashcard/CreateFlashcards.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+
+export function CreateFlashcards() {
+  
+  return(
+  <div className="min-h-screen bg-ctp-base">
+    <p>CreacteFlashcards</p>
+  </div>
+  );
+}
--- src/pages/flashcard/Flashcards.jsx
+++ src/pages/flashcard/Flashcards.jsx
@@ -1,10 +1,34 @@
 import { useState, useEffect } from "react";
+import { Flashcard } from "../../components/Flashcard.jsx";
 
 export function Flashcards() {
+  const [index, setIndex] = useState(0);
+  const items = [
+    {
+      title: "Title",
+      content: "content content content content content",
+    },
+    {
+      title: "Title 2",
+      content: "content content content content content",
+    },
+    {
+      title: "Title 3",
+      content: "content content content content content",
+    },
+    {
+      title: "Title 4",
+      content: "content content content content content",
+    }
+  ];
   
   return(
-  <div>
-    <p>Flashcards</p>
+  <div className="flex justify-center items-center min-h-screen bg-ctp-base text-ctp-text overflow-hidden">
+    <ul className="relative bg-ctp-green flex justify-center items-center">
+      {items.map((item, index) => {
+        return <li key={index}><Flashcard title={item.title} content={item.content} index={index} isCard={true} /></li>
+      })}
+    </ul>
   </div>
   );
 }
