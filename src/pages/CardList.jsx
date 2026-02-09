a5f35583ff404527c8683feadfbfc34c755d602a
Theo <prstylukman@gmail.com>
2026-02-09 21:44:11 +0700

    edit text card

--- src/pages/CardList.jsx
+++ src/pages/CardList.jsx
@@ -7,7 +7,7 @@ export function CardList({user}) {
   return(
     <section className="min-h-screen bg-ctp-base flex flex-col gap-5 justify-center items-center">
       <Card link={'card/flashcards'} title="Flashcards" description="Better study with flashcards" />
-      <Card link={'card/create'} title="Create Flashcards" description="Create your own flashcards" />
+      <Card link={'card/create'} title="Create Flashcards" description="Edit and create your own flashcards" />
     </section>
   );
 }
